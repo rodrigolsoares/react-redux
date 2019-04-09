d9b0718bfca11bbc7e8b62b3703ac590b5fa6a98
RODRIGO-PC\Rodrigo <rodrigo.soares@bookhair.com.br>
2019-04-09 08:00:33 -0300

    exemplo preset react, e loader css

--- exemplo_spread/webpack.config.js
+++ exemplo_spread/webpack.config.js
@@ -1,5 +1,4 @@
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin'); 
 
 module.exports = {
     entry: './ex/index.js',
@@ -11,22 +10,15 @@ module.exports = {
         port:8080,
         contentBase: './public'
     }, 
-    plugins:[
-        new ExtractTextPlugin('app.css')
-    ],
     module: {
         loaders:[{
             test: /.js?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query:{
-                presets: ['es2015', 'react'], 
+                presets: ['es2015'], 
                 plugins: ['transform-object-rest-spread']
             }
-        },
-        {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader','css-loader')
         }]
     }
 }
