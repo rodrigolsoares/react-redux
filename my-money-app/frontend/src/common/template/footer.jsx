c6e56ccf75094ec9d0dbbf3b6a37f9ad608753b8
RODRIGO-PC\Rodrigo <rodrigo.soares@bookhair.com.br>
2019-05-02 18:12:49 -0300

    Criando frontend my-money-app

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default props => (
+
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2019
+            <a href='#' target='_blank'> TI R&R Soares</a>.
+        </strong>
+    </footer>
+)
---
+++ my-money-app/frontend/src/main/app.jsx
@@ -0,0 +1,20 @@
+import '../common/template/dependencies'
+
+import React from 'react'
+
+import Header from '../common/template/header'
+import Sidebar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
+
+export default props => (
+
+    <div className='wrapper'>
+        <Header />
+        <Sidebar />
+        <div className='content-wrapper'>
+            <h1>APP</h1>
+        </div>
+        <Footer />
+    </div>
+
+)
